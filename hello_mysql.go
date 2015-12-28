1edfd052250a14635c9da147fbb131ce10a9a6b1
Shawn Page <shawn.page@gmail.com>
2015-12-28 11:38:59 -0500

    cleanup - remove commented out code

--- hello_mysql.go
+++ hello_mysql.go
@@ -33,19 +33,6 @@ func main() {
 	latitude, longitude := 35.779590, -78.638179
 	email := "spage@webassign.net"
 
-	// insert_string := "INSERT INTO test.hello(world, email, latitude, longitude) VALUES('hello world!', 'spage@webassign.net', "
-	// insert_string += strconv.FormatFloat(latitude, 'f', -1, 64)
-	// insert_string += ", "
-	// insert_string += strconv.FormatFloat(longitude, 'f', -1, 64)
-	// insert_string += " )"
-
-	// fmt.Println(insert_string) //debug
-
-	// res, err := db.Exec(insert_string)
-	// fmt.Printf("email: %s\n", email)
-	// fmt.Printf("longitude: %f\n", longitude)
-	// fmt.Printf("latitude: %f\n", latitude)
-
 	res, err := db.Exec("INSERT INTO test.hello(world, email, latitude, longitude) VALUES(?, ?, ?, ?)", "hello world!", email, latitude, longitude)
 	if err != nil {
 		//log.Fatal("insert error:")
@@ -64,11 +51,6 @@ func main() {
 	}
 	defer rows.Close()
 
-	// cols, err := rows.Columns()
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
 	for rows.Next() {
 		var (
 			s1 string
@@ -77,45 +59,6 @@ func main() {
 			f1 float64
 			f2 float64
 		)
-
-		// Suppose the row contains ["hello", NULL, 12345, "12345.6789", "not-a-float"]
-		// cols, err := rows.Columns()
-		// cols, err := rows.Columns()
-		// if err != nil {
-		// 	log.Fatal(err)
-		// }
-
-		// fmt.Println("cols:") //debug
-		// fmt.Println(cols)    // debug
-		// fmt.Println("len cols:")
-		// fmt.Println(len(cols))
-
-		// dest := []interface{}{
-		// 	new(int32),
-		// 	new(string),
-		// 	new(string),
-		// 	new(float64),
-		// 	new(float64),
-		// }
-		// fmt.Println(dest[:len(cols)]) // debug
-
-		//err = rows.Scan(dest[:len(cols)]) // Could not get this to work.
-
-		// vals := make([]interface{}, len(cols))
-
-		// for i, _ := range cols {
-		// 	vals[i] = new(sql.RawBytes)
-		// }
-
-		// for rows.Next() {
-		// 	err = rows.Scan(vals...)
-		// 	if err != nil {
-		// 		// log.Fatal("scan error:") // debug
-		// 		log.Fatal(err)
-		// 	}
-
-		// }
-		//		log.Printf("%q %#v %d %f %f", s1, s2, i1, f1, f2)
 
 		err = rows.Scan(&i1, &s1, &s2, &f1, &f2)
 
